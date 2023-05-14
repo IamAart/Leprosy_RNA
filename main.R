17597fd1bbd920c5cffb64d01f8add7223911e8d
IamAart <arosmolen2505@gmail.com>
2023-05-14 17:01:08 +0200

    Created a function to puts RDA file into a variable

--- main.R
+++ main.R
@@ -1,1 +1,16 @@
 library(DESeq2)
+
+# GLOBAL VARIABLES
+COUNT_DATA_FILENAME <- "sasc326.rda"
+
+load_one_rdata_object <- function(file_path) {
+    # Gets one object from rdata (in our case "gte") and returns it
+    # Benefit: variable name can be defined and used easily
+    res <- local({
+        load(file_path)
+        return(get(ls()))
+    })
+    return(res)
+}
+
+count_data <- load_one_rdata_object(COUNT_DATA_FILENAME)
