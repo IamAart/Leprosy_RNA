9b2945acedd66900873b66e07336a520b17a6f75
IamAart <arosmolen2505@gmail.com>
2023-05-20 14:57:10 +0200

    Added a function to do DGE with edgeR

--- main.R
+++ main.R
@@ -1,12 +1,14 @@
 library(DESeq2)
 library(ggplot2)
+library(edgeR)
 
 # GLOBAL VARIABLES
 COUNT_DATA_FILENAME <- "data/sasc326.rda"
 COUNTS_VARIABLE_NAME <- "countsMatrix"
 SAMPLES_VARIABLE_NAME <- "samplesData"
 FEATURES_VARIABLE_NAME <- "featuresData"
-DESIGN <- ~ type
+# DESIGN <- ~ type
+DESIGN <- ~ group + YOB + gender
 
 load_one_rdata_object <- function(file_path) {
     # Gets one object from rdata (in our case "gte") and returns it
@@ -34,12 +36,40 @@ create_deseq_result <- function(count_data, design) {
     # filtered_feature_data <- count_data[[FEATURES_VARIABLE_NAME]][rownames(count_data[[COUNTS_VARIABLE_NAME]]),]
     # apply feature data in the DESeqDataSet
     # mcols(dds) <- DataFrame(mcols(dds), count_data[[FEATURES_VARIABLE_NAME]])
-    result <- results(DESeq(dds))
+    deseq <- DESeq(dds)
+    deseq
+}
+
+create_edgeR_result <- function(count_data) {
+    # put gene name into rownames
+    rownames(count_data[[COUNTS_VARIABLE_NAME]]) <- count_data[[COUNTS_VARIABLE_NAME]][,1]
+    count_data[[COUNTS_VARIABLE_NAME]][,1] <- NULL
+
+    dge_list <- DGEList(
+        counts = count_data[[COUNTS_VARIABLE_NAME]],
+        samples = count_data[[SAMPLES_VARIABLE_NAME]],
+        # genes = count_data[[FEATURES_VARIABLE_NAME]]
+    )
+    # filter by genes with low counts
+    keep <- filterByExpr(y = dge_list)
+    dge <- dge_list[keep, , keep.lib.sizes = FALSE]
+    #normalization
+    dge <- calcNormFactors(dge)
+    # estimating dispersions with qCML
+    disp <- estimateDisp(dge)
+
+    # Check DGE
+    et <- exactTest(disp)
+    et
+    # top_dge <- topTags(et, n="Inf")
+    # print(summary(decideTests(object = et, lfc = 1)))
+
 }
 
 count_data <- load_one_rdata_object(COUNT_DATA_FILENAME)
-test <- create_deseq_result(count_data, DESIGN)
 
-# showcase results
-summary(test)
-plotMA(test, ylim=c(-2,2))
+# edgeR
+# plotMD(create_loom_result(count_data))
+
+# DESeq2
+# DESeq2::plotMA(results(create_deseq_result(count_data, DESIGN)))
