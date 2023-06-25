bb35594bbff75466247899adf5e49365b19eec44
IamAart <arosmolen2505@gmail.com>
2023-06-25 15:52:39 +0200

    Fixed the design parameter and showcase of the result

--- main.R
+++ main.R
@@ -3,14 +3,6 @@ library(ggplot2)
 library(edgeR)
 library(Glimma)
 
-# GLOBAL VARIABLES
-COUNT_DATA_FILENAME <- "./data/sasc326.rda"
-COUNTS_VARIABLE_NAME <- "countsMatrix"
-SAMPLES_VARIABLE_NAME <- "samplesData"
-FEATURES_VARIABLE_NAME <- "featuresData"
-DESIGN <- ~ groupFirst + groupHHC + type + YOB + gender
-
-
 load_one_rdata_object <- function(file_path) {
     # Gets one object from rdata (in our case "gte") and returns it
     # Benefit: variable name can be defined and used easily
@@ -35,26 +27,28 @@ equalize_feature_counts <- function(counts, features, adjust_common) {
         common <- Reduce(intersect, lapply((list(counts, features)), row.names))
         return(list(counts = counts[common, ], features = features[common, ]))
     }
-    return(list(counts = counts, features = features))
+    return(c(counts = counts, features = features))
 }
 
 remove_rowData_dup_colnames <- function(data) {
     # rowData cannot have columns named "seqnames", "ranges", "strand", "start", "end", "width", "element"
-    forbidden_colnames <- list("seqnames", "ranges", "strand", "start", "end", "width", "element")
-    replace_colnames <- list("sequence_names", "seq_ranges", "seq_strand", "start_loc", "end_loc", "width_size", "seq_element")
-    for (i in len(forbidden_colnames)) {
-        names(data)[names(data) == forbidden_colnames[i]] <- replace_colnames[i]
+    forbidden_colnames <- c("seqnames", "ranges", "strand", "start", "end", "width", "element")
+    replace_colnames <- c("sequence_names", "seq_ranges", "seq_strand", "start_loc", "end_loc", "width_size", "seq_element")
+    for (i in seq_along(forbidden_colnames)) {
+        names(data)[names(data) == forbidden_colnames[[i]]] <- replace_colnames[[i]]
     }
     return(data)
 }
 
-prepare_DGE_data <- function(data, dge_type, adjust_common, include_features, c_name, f_name, s_name) {
+prepare_DGE_data <- function(data, dge_type, adjust_common, include_features, design, c_name, f_name, s_name) {
     # prepare the dataset with the features column as rownames in order to match it with features dataset
     counts <- column_to_rownames(data[[c_name]], 1)
     features <- column_to_rownames(data[[f_name]], 1)
 
     # find the genes with the same name in both feature and count data
-    list[counts, features] <- equalize_feature_counts(counts, features, adjust_common)
+    c_f <- equalize_feature_counts(counts, features, adjust_common)
+    counts <- c_f[["counts"]]
+    features <- c_f[["features"]]
     if (dge_type == "Limma-Voom" || dge_type == "EdgeR") {
         if (include_features) {
             return(DGEList(counts = counts, samples = data[[s_name]], genes = features))
@@ -64,10 +58,10 @@ prepare_DGE_data <- function(data, dge_type, adjust_common, include_features, c_
     } else if (dge_type == "DESeq2") {
         if (include_features) {
             features <- remove_rowData_dup_colnames(features)
-            return(DESeqDataSetFromMatrix(countData = counts, colData = data[[s_name]], rowData = features, design = DESIGN))
+            return(DESeqDataSetFromMatrix(countData = counts, colData = data[[s_name]], rowData = features, design = design))
             return(dds)
         } else {
-            return(DESeqDataSetFromMatrix(countData = counts, colData = data[[SAMPLES_VARIABLE_NAME]], design = DESIGN))
+            return(DESeqDataSetFromMatrix(countData = counts, colData = data[[SAMPLES_VARIABLE_NAME]], design = design))
 
         }
     } else {
@@ -75,62 +69,70 @@ prepare_DGE_data <- function(data, dge_type, adjust_common, include_features, c_
     }
 }
 
-perform_DGE_analysis <- function(data, dge_type, adjust_common, include_features, c_name, f_name, s_name) {
-    dge_data <- prepare_DGE_data(data, dge_type, adjust_common, include_features, c_name, f_name, s_name)
+perform_DGE_analysis <- function(data, dge_type, adjust_common, include_features, design, c_name, f_name, s_name) {
+    dge_data <- prepare_DGE_data(data, dge_type, adjust_common, include_features, design, c_name, f_name, s_name)
     # TODO: return data for table, plot and/or more + save table data in file
-    # TODO: Adjust contrast within each function, maybe add the contrast by variable
-    # TODO: Add this or update this in the Limma-Voom and EdgeR procedure. It should come between dge_data and the first sentence in the if statement
-    # dge <- calcNormFactors(dge_list)
-    #
-    # keep <- filterByExpr(y = dge)
-    # dge <- dge[keep, , keep.lib.sizes = FALSE]
-    # dge
+
     if (dge_type == "DESeq2") {
-        contr <- c("HHC vs First", "groupHHC", "groupFirst") # TODO: Fix the contrast
+        contr <- c("group", "HHC", "First") # This is used to showcase the result of the difference in gene expression between First and HHC
         deseq <- DESeq(dge_data)
-        deseq_results <- results(deseq, contrast = contr, alpha = 0.05)
-        return(deseq_results)
-    } else if (dge_type == "Limma-Voom") {
-        design <- model.matrix(DESIGN, data[[s_name]])
-        v <- voom(dge_data, design, plot=TRUE)
-        fit <- lmFit(v, design)
-        # Make contrast here
-        tmp <- eBayes(fit, robust = TRUE)
-        return(tmp)
-    } else if (dge_type == "EdgeR") {
-        dge <- calcNormFactors(dge_data)
-        design <- model.matrix(DESIGN, count_data[[s_name]])
-        rownames(design) <- colnames(dge)
-        disp <- estimateDisp(dge, design)
-        et <- exactTest(disp)
-        return(et)
+        result <- results(deseq, contrast = contr, alpha = 0.05)
+        return(result)
     } else {
-        stop("dge_type parameter should be either 'Limma-Voom', 'EdgeR' or 'DESeq2'")
+        group <- factor(dge_data$group)
+        design <- model.matrix(~ 0 + group, data[[s_name]])
+        colnames(design) <- levels(data[[s_name]]$group)
+        # design <- model.matrix(design, data[[s_name]])
+        # colnames(design) <- c(levels(data[[s_name]]$group), "YOB", "gender")
+        dge_data <- calcNormFactors(dge_data)
+        # keep <- filterByExpr(y = dge_data, design)
+        # dge_data <- dge_data[keep, , keep.lib.sizes = FALSE]
+        if (dge_type == "Limma-Voom") {
+            v <- voom(dge_data, design, plot=TRUE)
+            fit <- lmFit(v, design)
+            contrast <- makeContrasts(First_vs_HHC = HHC - First, levels = v$design)
+            fit <- contrasts.fit(fit, contrasts = contrast)
+            result <- eBayes(fit)
+            return(result)
+        } else if (dge_type == "EdgeR") {
+            # TODO fix: contrast
+            disp <- estimateDisp(dge_data, design)
+            fit <- glmQLFit(disp, design)
+            contrast <- makeContrasts(First_vs_HHC = HHC - First, levels = design)
+            result <- glmQLFTest(fit, contrast = contrast[, "First_vs_HHC"])
+            # et <- exactTest(disp)
+            return(result)
+        } else {
+            stop("dge_type parameter should be either 'Limma-Voom', 'EdgeR' or 'DESeq2'")
+        }
     }
 }
 
+COUNT_DATA_FILENAME <- "./data/sasc326.rda"
+COUNTS_VARIABLE_NAME <- "countsMatrix"
+SAMPLES_VARIABLE_NAME <- "samplesData"
+FEATURES_VARIABLE_NAME <- "featuresData"
+DGE_TYPE <- "EdgeR" # Can be "DESeq2", "Limma-Voom" or "EdgeR"
+ADJUST_COMMON <- TRUE
+INCLUDE_FEATURES <- TRUE
+
+DESIGN <- ~ group + YOB + gender
+
 count_data <- load_one_rdata_object(COUNT_DATA_FILENAME)
-count_data[[SAMPLES_VARIABLE_NAME]]['groupFirst'] <- ifelse(count_data[[SAMPLES_VARIABLE_NAME]]$group=="First",1,0)
-count_data[[SAMPLES_VARIABLE_NAME]]['groupSecond'] <- ifelse(count_data[[SAMPLES_VARIABLE_NAME]]$group=="Second",1,0)
-count_data[[SAMPLES_VARIABLE_NAME]]['groupHHC'] <- ifelse(count_data[[SAMPLES_VARIABLE_NAME]]$group=="HHC",1,0)
-count_data[[SAMPLES_VARIABLE_NAME]]['typeMB'] <- ifelse(count_data[[SAMPLES_VARIABLE_NAME]]$type=="MB",1,0)
-count_data[[SAMPLES_VARIABLE_NAME]]['typePB'] <- ifelse(count_data[[SAMPLES_VARIABLE_NAME]]$type=="PB",1,0)
-count_data[[SAMPLES_VARIABLE_NAME]]['typeHHC'] <- ifelse(count_data[[SAMPLES_VARIABLE_NAME]]$type=="HHC",1,0)
+dge_result <- perform_DGE_analysis(
+    data = count_data,
+    dge_type = DGE_TYPE,
+    adjust_common = ADJUST_COMMON,
+    include_features = INCLUDE_FEATURES,
+    design = DESIGN,
+    c_name = COUNTS_VARIABLE_NAME,
+    f_name = FEATURES_VARIABLE_NAME,
+    s_name = SAMPLES_VARIABLE_NAME
+)
+# TODO: save data as csv
+# TODO: showcase in different kind of figures: MA plot, Volcano Plot, Heatmap, p-value Histogram
 
-# edgeR
-# dge_result <- create_edgeR_result(count_data, FALSE, FALSE)
-
-# Limma Voom
-# dge_result <- create_limma_voom_result(count_data, TRUE, TRUE)
-
-#DESeq2
-# dge_result <- create_deseq_result(count_data, TRUE, TRUE)
-
-# TODO: Showcase results in the same way (thinking Heatmap and Histogram)
-# TODO: Pick column DGE you would like to see plotted
 # glimmaMA(dge_result)
-
-# showcase results in a Volcano plot
 # glimmaVolcano(dge_result)
 
 # TODO: Optimize Design
