d0b21ec5fd5ab986be03c901f5ea5f8abdb4f2ae
IamAart <arosmolen2505@gmail.com>
2024-11-26 11:09:34 +0100

    Added Final Results

--- data/see_python.py
+++ data/see_python.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 data = pd.read_excel("data/Current_Comparison_SVM_RF/rf_analysis_9_22.xlsx")
 data = data.sort_values("auc", ascending=False)
+data = data[data["auc"]>=0.96]
 gene_names = dict()
 ensembles = dict()
 print(data.head)
 
-index = 1
 for _, row in data.iterrows():
     e_list = row["ensembles"].split(", ")
     g_list = row["Gene names"].split(", ")
@@ -17,11 +17,10 @@ for _, row in data.iterrows():
         else: 
             ensembles[e_list[i]] += 1
             gene_names[g_list[i]] += 1
-    
-    if index == 25:
-        break
-    index += 1
-# print(ensembles)
-print({k: v for k, v in sorted(gene_names.items(), key=lambda item: item[1], reverse=True)})
-print(sorted(ensembles, key=lambda k: ensembles[k], reverse=True)[0:22])
-print(sorted(gene_names, key=lambda k: gene_names[k], reverse=True)[0:22])
+
+ensemble_list = sorted(ensembles, key=lambda k: ensembles[k], reverse=True)[0:20]
+limmavoom = pd.read_excel("data/LimmaVoom/All_GENES_LimmaVoom_RNA_SEQ.xlsx")
+results = limmavoom[limmavoom["Row.names"].isin(ensemble_list)]
+sorted_values = results.sort_values("P.Adjust", ascending=True)
+sorted_values = sorted_values[["gene_name", "Row.names", "biotype"]]
+print(sorted_values)
