c783064c09a0e97fcd255ae162a717ec5ef225e0
IamAart <arosmolen2505@gmail.com>
2024-12-08 19:39:30 +0100

    Streamlined the process for DGE analysis and MDS plot creation

---
+++ DGE_Analysis.R
@@ -0,0 +1,63 @@
+library("limma")
+library("edgeR")
+library("DESeq2")
+library("dplyr")
+source("common.R")
+
+# These global variables are gathered from the .env file in the common.R file
+data <- prepare_data(FILEPATH, COUNTS_NAME, FEATURES_NAME, SAMPLES_NAME)
+counts <- data[["counts"]]
+features <- data[["features"]]
+samples <- data[["samples"]]
+
+# Make sure the order of the rownames and colnames is the same
+counts <- check_order_rownames(samples, counts)
+if (is.logical(counts)) {
+  stop("Error when adjusting colnames and rownames to be similar")
+}
+
+if (DGE_METHOD == "DESeq2") {
+  dds <- DESeqDataSetFromMatrix(countData = counts, colData = samples, design = ~ group)
+  # Create MDS plot if selected
+  if (BOOL_MDS_PLOT) {
+    sf <- estimateSizeFactors(dds)
+    normalized_data <- DESeq2::counts(sf, normalized=TRUE)
+    normalized_data <- log2(normalized_data)
+    make_mds_plot(normalized_data, sprintf("./data/Plots/%s_MDS_plot.png", DGE_METHOD))
+  }
+  # Perform DESEQ2
+  deseq <- DESeq(dds)
+  res <- results(deseq, c("group", "First", "HHC"), pAdjustMethod = "BH")
+} else {
+  # Create constrast parameter (HHC vs First)
+  design <- model.matrix(~0+factor(samples$group, c("HHC", "First", "Second")))
+  colnames(design) <- c("HHC", "First", "Second")
+  contrast <- makeContrasts(First_vs_HHC = First - HHC, levels = design)
+
+  # Normalize data
+  normalized_data <- normalization_edgeR(counts, samples, design, features)
+  # Create MDS plot if selected
+  if (BOOL_MDS_PLOT) { make_mds_plot(normalized_data, sprintf("./data/Plots/%s_MDS_plot.png", DGE_METHOD)) }
+  if (DGE_METHOD == "LimmaVoom") {
+    # Prepare data for limma voom
+    voom <- voom(normalized_data, design, plot=FALSE, normalize="quantile")
+    fit <- lmFit(voom, design)
+    fit <- contrasts.fit(fit, contrasts = contrast)
+    # Perform DGE Analysis
+    ebayes <- eBayes(fit)
+    res <-  topTable(ebayes, n=Inf, adjust.method = "BH")
+  }
+  else if (DGE_METHOD == "EdgeR") {
+    # Prepare data for edgeR
+    disp <- estimateGLMCommonDisp(normalized_data,design)
+    disp <- estimateGLMTrendedDisp(disp, design, method="power")
+    disp <- estimateGLMTagwiseDisp(disp, design)
+    fit <- glmFit(disp, design)
+    # Perform DGE Analysis
+    lrt <- glmLRT(fit, contrast=contrast)
+    res<- as.data.frame(topTags(lrt, n=Inf, adjust.method = "BH"))
+  }
+}
+
+# Save results in table
+res <- make_save_table(res, features, sprintf("./data/%s/TEST_%s_RNA_SEQ", DGE_METHOD, DGE_METHOD))
--- common.R
+++ common.R
@@ -12,11 +12,13 @@ CODING_BIOTYPES <- as.vector(strsplit(Sys.getenv("CODING_BIOTYPES"), ","))
 CUT_OFF <- as.numeric(Sys.getenv("CUT_OFF"))
 LOG2_OFF <- log2(as.numeric(Sys.getenv("LOG2_OFF")))
 
-FILEPATH <- "./data/sasc326.rda"
-COUNTS_NAME <- "countsMatrix"
-FEATURES_NAME <- "featuresData"
-SAMPLES_NAME <- "samplesData"
-VENN_NAMES <- "gene_name" # "gene_name" or "Row.names"
+FILEPATH <- Sys.getenv("FILEPATH")
+COUNTS_NAME <- Sys.getenv("COUNTS_NAME")
+FEATURES_NAME <- Sys.getenv("FEATURES_NAME")
+SAMPLES_NAME <- Sys.getenv("SAMPLES_NAME")
+
+DGE_METHOD <- Sys.getenv("DGE_METHOD")
+BOOL_MDS_PLOT <- as.logical(Sys.getenv("BOOL_MDS_PLOT"))
 
 load_one_rdata_object <- function(file_path) {
     # Gets one object from rdata (in our case "gte") and returns it
@@ -30,10 +32,10 @@ load_one_rdata_object <- function(file_path) {
     return(res)
 }
 
-column_to_rownames <- function(df, column_number) {
+column_to_rownames <- function(df, column_name) {
     # Function to swap the first column to the rownames
-    rownames(df) <- df[,column_number]
-    df[,column_number] <- NULL
+    rownames(df) <- df[,column_name]
+    df[,column_name] <- NULL
     df
 }
 
@@ -50,73 +52,78 @@ check_order_rownames <- function (data1, data2) {
     return(data2)
 }
 
-prepare_data <- function(path, counts_name, features_name, samples_name, biotypes) {
+prepare_data <- function(path, counts_name, features_name, samples_name) {
     # Prepare data so that it easy to use during normalization
     # It adjusts the rownames to the column containing the ensemble ids or the samples to the rownames. It also selects all non-coding genes
     # Returns a list containing the 3 data structures counts, samples and features
     data <- load_one_rdata_object(path)
-
-    counts <- column_to_rownames(data[[counts_name]], 1)
-    features <- column_to_rownames(data[[features_name]], 1)
+    counts <- column_to_rownames(data[[counts_name]], "feature")
+    features <- column_to_rownames(data[[features_name]], "feature")
     counts <- merge(counts, features, by = 'row.names')
+    counts <- column_to_rownames(counts, "Row.names")
 
     samples <- data[[samples_name]]
     samples <- tibble::column_to_rownames(samples, "sample")
     return(list(counts = counts, features = features, samples = samples))
 }
 
-normalization_edgeR <- function(counts, samples, design) {
+make_mds_plot <- function(norm_data, plot_path) {
+    colors <- c("#88ccee", "#44aa99", "#117733", "#332288", "#ddcc77", "#999933", "#cc6677", "#882255", "#aa4499", "#dddddd")
+
+    plot <- plotMDS(norm_data)
+    plot <- as.data.frame(plot)
+    plot_data <- plot[, c("x", "y")]
+    plot_data["Row.names"] <- rownames(plot_data)
+    samples["Row.names"] <- rownames(samples)
+
+    hhc <- samples[samples$group == "HHC", ]$Row.names
+    t1 <- samples[samples$group == "First", ]$Row.names
+    data <- plot_data %>%
+        mutate( Colorcode = case_when(
+            Row.names %in% hhc ~ "HHC",
+            Row.names %in% t1 ~ "First",
+            TRUE ~ "Second"
+        ) )
+    data$Colorcode <- factor(data$Colorcode, levels = c("HHC", "First", "Second"))
+
+    text_repel <- data
+    # text_repel <- data[data$x <= -1.25, ]
+    # text_repel <- data[data$Row.names %in% c("s103830.003.011", "s103830.004.017", "s103830.003.003"), ]
+    p <- ggplot(data, aes(x=x, y=y, label = Row.names, color=Colorcode)) +
+            geom_point(size=3) +
+            geom_text_repel(data = text_repel, size=7) +
+            theme_minimal() +
+            labs(x = "Log2 Fold Change Dimension 1", y="Log 2 Fold Change Dimension 2") +
+            scale_color_manual(
+                values = c(HHC = colors[1], First = colors[4], Second = colors[7]),
+                labels = c(HHC = "Healthy", First = "Progressors t=1", Second = "Progressors t=2")
+            ) +
+            guides(color=guide_legend("Patient Information")) +
+            theme(
+                # Axis titles font size
+                axis.title.x = element_text(size = 19),    # x-axis label size
+                axis.title.y = element_text(size = 19),    # y-axis label size
+
+                # Axis tick labels font size
+                axis.text.x = element_text(size = 17),     # x-axis ticks (labels) size
+                axis.text.y = element_text(size = 17),     # y-axis ticks (labels) size
+
+                # Legend title and text font size
+                legend.title = element_text(size = 17),    # Legend title size
+                legend.text = element_text(size = 16)      # Legend labels size
+            )
+    ggsave(plot_path, p, width=15, height=10, dpi=720)
+}
+
+normalization_edgeR <- function(counts, samples, design, features) {
     # Normalize data from of the counts and sample and put them into a DGE List before going into the DGE Analysis
     normalized_data <- DGEList(counts = counts, samples = samples)
-    # TODO this reduces the amount of rows to 17.000
+
+    # This reduces the amount of rows to 17.000, however this in order with the EdgeR vigenette
     keep <- filterByExpr(y = normalized_data, design)
-    normalized_data <- normalized_data[keep, keep.lib.sizes = FALSE]
+    normalized_data <- normalized_data[keep, ,keep.lib.sizes = FALSE]
 
     normalized_data <- calcNormFactors(normalized_data, method = "TMM")
-
-    # colors = c("#88ccee", "#44aa99", "#117733", "#332288", "#ddcc77", "#999933", "#cc6677", "#882255", "#aa4499", "#dddddd")
-
-    # plot <- plotMDS(normalized_data)
-    # plot <- as.data.frame(plot)
-    # plot_data <- plot[, c("x", "y")]
-    # plot_data["Row.names"] <- rownames(plot_data)
-    # samples["Row.names"] <- rownames(samples)
-
-    # hhc <- samples[samples$group == "HHC", ]$Row.names
-    # t1 <- samples[samples$group == "First", ]$Row.names
-    # data <- plot_data %>%
-    #     mutate( Colorcode = case_when(
-    #         Row.names %in% hhc ~ "HHC",
-    #         Row.names %in% t1 ~ "First",
-    #         TRUE ~ "Second"
-    #     ) )
-    # data$Colorcode <- factor(data$Colorcode, levels = c("HHC", "First", "Second"))
-
-    # text_repel <- data[data$x <= -1.25, ]
-    # p <- ggplot(data, aes(x=x, y=y, label = Row.names, color=Colorcode)) +
-    #         geom_point(size=3) +
-    #         geom_text_repel(data = text_repel, size=7) +
-    #         theme_minimal() +
-    #         labs(x = "Log2 Fold Change Dimension 1", y="Log 2 Fold Change Dimension 2") +
-    #         scale_color_manual(
-    #             values = c(HHC = colors[1], First = colors[4], Second = colors[7]),
-    #             labels = c(HHC = "Healthy", First = "Progressors t=1", Second = "Progressors t=2")
-    #         ) +
-    #         guides(color=guide_legend("Patient Information")) +
-    #         theme(
-    #             # Axis titles font size
-    #             axis.title.x = element_text(size = 19),    # x-axis label size
-    #             axis.title.y = element_text(size = 19),    # y-axis label size
-
-    #             # Axis tick labels font size
-    #             axis.text.x = element_text(size = 17),     # x-axis ticks (labels) size
-    #             axis.text.y = element_text(size = 17),     # y-axis ticks (labels) size
-
-    #             # Legend title and text font size
-    #             legend.title = element_text(size = 17),    # Legend title size
-    #             legend.text = element_text(size = 16)      # Legend labels size
-    #         )
-    # ggsave("./MDS_Plot.png", p, width=15, height=10, dpi=720)
     return(normalized_data)
 }
 
